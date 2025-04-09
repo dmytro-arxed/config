0b9f30aa2556c50959fb9626b9ed3c5a6b762dd0
Dmytro <Dmytro@arxed.com>
2025-04-09 10:12:24 -0400

    keymap for opening file explorer in a pan

--- init.lua
+++ init.lua
@@ -37,9 +37,16 @@ if not vim.loop.fs_stat(lazypath) then
   }
 end
 
+vim.opt.termguicolors = true
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function ()
+      require 'colorizer'.setup()
+    end
+  },
   {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
@@ -368,6 +375,7 @@ vim.keymap.set('n', '<leader>hm', ':lua require("harpoon.ui").toggle_quick_menu(
 vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = '[Q]uit' })
 vim.keymap.set('n', '<leader>l', ':noh<CR>', { desc = 'remove high[L]ight', silent = true })
 vim.keymap.set('n', '<leader>e', ':Neotree position=float reveal=true<CR>', { desc = '[E]xplorer here' })
+vim.keymap.set('n', '<leader>fe', ':vsplit<CR><C-w>l:Neotree position=current reveal=true<CR>', { desc = '[E]xplorer here' })
 
 -- begin Terminal
 vim.keymap.set('t', '<leader>q', '<C-\\><C-n>:q<CR>', { desc = 'Quit insert mode in terminal' })
